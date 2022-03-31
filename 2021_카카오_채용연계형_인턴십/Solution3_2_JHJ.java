8255ae307bc478d80979d5408c6fea1f10598785
Jung Hojin <hojinnjung@gmail.com>
2022-03-31 13:27:31 +0900

    2021 카카오 채용연계형 인턴십 - 표편집
    두 가지 방법으로 푼 표편집

---
+++ 2021_카카오_채용연계형_인턴십/Solution3_2_JHJ.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Stack;
+
+class Solution {
+	public String solution(int n, int k, String[] cmd) {
+		String answer = "";
+		LinkedList<Integer> table = new LinkedList<Integer>();
+		Stack<Integer> history = new Stack<Integer>();
+
+		for (int i = 0; i < n; i++) {
+			table.add(i);
+		}
+
+		int curIdx = k;
+		for (String each : cmd) {
+			char order = each.charAt(0);
+			int cnt = 0;
+			switch (order) {
+			case 'D':
+				cnt = Integer.parseInt(each.split(" ")[1]) % n;
+				curIdx = (curIdx + cnt) >= n ? (curIdx + cnt) % n : (curIdx + cnt);
+				break;
+
+			case 'U':
+				cnt = Integer.parseInt(each.split(" ")[1]) % n;
+				curIdx = (curIdx - cnt) < 0 ? n + (curIdx - cnt) : (curIdx - cnt);
+				break;
+
+			case 'C':
+				history.push(curIdx);
+				if (curIdx == --n)
+					curIdx--;
+				break;
+
+			case 'Z':
+				int item = history.pop();
+				if (item <= curIdx)
+					++curIdx;
+				n++;
+				break;
+			}
+		}
+
+		// 출력
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < n; i++)
+			builder.append("O");
+		while (!history.isEmpty()) {
+			int index = history.pop();
+			builder.insert(index, "X");
+		}
+
+		return answer = builder.toString();
+	}
+}
+
+public class Solution3_2_JHJ {
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(
+				sol.solution(8, 2, new String[] { "D 2", "C", "U 3", "C", "D 4", "C", "U 2", "Z", "Z", "U 1", "C" }));
+	}
+}
