3a873741c64a7e3f823f9ede42c4af467e27be99
Ellen <54658745+Seunghui98@users.noreply.github.com>
2022-06-13 23:31:13 +0900

    2022 KAKAO BLIND RECRUITMENT - k진수에서소수개수구하기 - 구현

---
+++ 2022_KAKAO_BLIND_RECRUITMENT/Solution2_LSH.java
@@ -0,0 +1,42 @@
+// 2022 KAKAO BLIND RECRUITMENT - k진수에서소수개수구하기
+// 구현
+
+import java.util.*;
+
+class Solution2_LSH {
+    public static void main(String[] args){
+    }
+    
+    public int solution(int n, int k) {
+        int answer = 0;
+        String num = Integer.toString(n, k);
+        String[] num_array = num.split("0");
+
+        for(String s:num_array){
+            if(s.equals("")) continue;
+            long number = Long.parseLong(s);
+            if(isPrime(number)){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    
+    public static boolean isPrime(long num){
+        if(num == 1){
+            return false;
+        }
+        
+        if(num == 2) return true;
+        
+        for(int i=2;i<=(int)Math.sqrt(num);i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    
+    
+}
